602728bb9cda7bd0b4f69e07ec8ae4911bc381f2
mac <349996772@qq.com>
2018-11-09 21:46:48 +0800

    521.1314 mean 'I love you for life'

---
+++ formula_of_romance.rb
@@ -0,0 +1,21 @@
+require 'proc_compose'
+
+# 浪漫的表白公式
+# expression formula of romance
+
+one = proc {|a| a+52.8}
+two = proc {|a| a*5}
+three = proc {|a| a-3.4343}
+four =proc{|a| a*2}
+five =proc{|a| (a*10)}
+
+# 和Haskell很相似的写法
+# just for chinese syntax.
+# 521.1314 mean "I love you for life"
+
+
+random=rand(0..100)
+
+p (four * three * two * one).(random)-five.(random)
+
+
